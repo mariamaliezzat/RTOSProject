e8d435d9e9026b5db4fb42bb8a2423f275a9b9f5
mariamaliezzat <mariamali.ezzat@gmail.com>
2021-07-06 09:23:42 +0200

    master

---
+++ Debug/FreeRTOS Source/portable/GCC/ARM_CM4F/port.d
@@ -0,0 +1,21 @@
+FreeRTOS Source/portable/GCC/ARM_CM4F/port.o: \
+ ../FreeRTOS\ Source/portable/GCC/ARM_CM4F/port.c \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/projdefs.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/portable.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/task.h \
+ C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/list.h
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/projdefs.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/portable.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/deprecated_definitions.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/mpu_wrappers.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/task.h:
+C\:\Users\ahmed\eclipse-workspace\Demo\FreeRTOS\ Source\include/list.h:
